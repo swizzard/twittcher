a20e408a52026d8c9d0bb42743ceabe85d33451a
Sam Raker <sam.raker@gmail.com>
2014-09-17 00:27:34 -0400

    setup.py update

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ exec(open('twittcher/version.py').read()) # loads __version__
 
 setup(name='twittcher',
       version=__version__,
-      author='Zulko',
+      author='swizzard',
     description=("Watch tweets on Twitter's user pages or search pages."),
     long_description=open('README.rst').read(),
     license='see LICENSE.txt',
     keywords="Twitter tweet search bot",
-    install_requires=['beautifulsoup'],
+    install_requires=['beautifulsoup', 'redis'],
     packages= find_packages(exclude='docs'))
